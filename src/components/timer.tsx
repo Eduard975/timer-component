0942c82abc8ac7510de7c49fdc618e5f7324ca42
Eduard975 <theblackasasin@yahoo.com>
2024-11-01 15:34:05 +0200

    timer works

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,17 +1,21 @@
 import { EndTimeOutOfBoundsError } from "./customError";
 import "../css/component.css"
+import { useEffect, useState } from "react";
+import { time } from "console";
 
 export type MyTimerProps = { title: string; endTime: number; elapsedTime?: number };
 
-function calcTime(time: number) {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = Math.floor(time % 60);
+function calcTimeFromMS(time: number) {
+    time = (time / 1000);
+    const minutes = ((time % 3600) / 60);
+    const seconds = (time % 60);
 
-    return { hours, minutes, seconds }
+
+    return `${minutes > 9 ? minutes.toFixed(0) : `0${minutes.toFixed(0)}`}`
+        + `:${seconds > 9 ? seconds.toFixed(0) : `0${seconds.toFixed(0)}`}`;
 }
 
-export const Timer = ({ title, endTime, }: MyTimerProps) => {
+export const Timer = ({ title, endTime }: MyTimerProps) => {
     if (endTime < 0) {
         throw new EndTimeOutOfBoundsError("EndTime can't be negative");
     }
@@ -20,7 +24,29 @@ export const Timer = ({ title, endTime, }: MyTimerProps) => {
         throw new EndTimeOutOfBoundsError("EndTime can't exceed 3599 seconds");
     }
 
-    const { minutes, seconds } = calcTime(endTime);
+    const [timerStarted, setTImerStarted] = useState(true)
+    const [startTime, setStartTime] = useState(Date.now())
+    const [currentTime, setCurrentTime] = useState(Date.now())
+    const endTimeMs = endTime * 1000 + startTime;
+
+    useEffect(() => {
+        if (!timerStarted) {
+            return;
+        }
+
+        const iId = setInterval(() => {
+            setCurrentTime(Date.now())
+        }, 1000);
+
+        return () => clearInterval(iId)
+    }, [timerStarted])
+
+    useEffect(() => {
+        if (currentTime >= endTimeMs) {
+            setTImerStarted(false)
+        }
+
+    }, [currentTime, endTimeMs])
 
     return (
         <div className="container">
@@ -28,11 +54,21 @@ export const Timer = ({ title, endTime, }: MyTimerProps) => {
                 {title}
             </p>
             <p className="timer">
-                {minutes}:{seconds}
+                {
+                    calcTimeFromMS((currentTime - startTime))
+                    //(endTime - (endTimeMs - currentTime) / 1000).toFixed(0)
+                }
             </p>
             <p className="timer-top-buttom">
-                {minutes}:{seconds} Left
+                {calcTimeFromMS((endTimeMs - currentTime))} Left
             </p>
+
+            <div className="buttons-row">
+                <button>Start</button>
+                <button>Pause</button>
+                <button>Stop</button>
+            </div>
+
         </div>
     );
 };
